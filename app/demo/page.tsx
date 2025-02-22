a0511e711ade43dac94fc6e8f090272d07b63b9c
mehul79 <mehulrgupta@gmail.com>
2025-02-22 17:11:33 +0530

    added suspence to demo page

--- app/demo/page.tsx
+++ app/demo/page.tsx
@@ -1,5 +1,6 @@
 "use client";
-import { useEffect, useRef, useState } from "react";
+import { Suspense, useEffect, useRef, useState } from "react";
+import LoadingSkeleton from "@/components/loading";
 
 export default function DemoPage() {
   const [position, setPosition] = useState(50);
@@ -42,6 +43,7 @@ export default function DemoPage() {
   }, []);
 
   return (
+    <Suspense fallback={<LoadingSkeleton />}>
     <div className="max-w-7xl mx-auto px-4 py-8 overflow-hidden">
       {/* Background Pattern */}
       <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]" />
@@ -52,7 +54,7 @@ export default function DemoPage() {
 
       <div
         ref={containerRef}
-        className="relative w-[800px] aspect-video rounded-lg mx-auto overflow-hidden"
+        className="relative w-full md:w-[800px] aspect-video rounded-lg mx-auto overflow-hidden"
       >
         {/* Before Video */}
         <div
@@ -126,5 +128,6 @@ export default function DemoPage() {
         </div>
       </div>
     </div>
+    </Suspense>
   );
 }
